b2c66a6756ab17c851510640effc0ecf7da40ef0
Sam Carter <sam.carter@education.gov.uk>
2024-01-22 14:39:02 +0000

    Added initial contentful setup and dummy models & views to test the integration

---
+++ src/Dfe.EarlyYearsQualification.Content/Services/ContentfulContentService.cs
@@ -0,0 +1,24 @@
+﻿using Contentful.Core;
+using Contentful.Core.Models;
+using Dfe.EarlyYearsQualification.Content.Entities;
+
+namespace Dfe.EarlyYearsQualification.Content.Services;
+
+public class ContentfulContentService : IContentService
+{
+    private readonly IContentfulClient _contentfulClient;
+
+    public ContentfulContentService(IContentfulClient contentfulClient)
+    {
+        _contentfulClient = contentfulClient;
+    }
+
+    public async Task<LandingPage> GetLandingPage()
+    {
+        var landingPageEntries = await _contentfulClient.GetEntriesByType<LandingPage>("landingPage");
+        var landingPageContent = landingPageEntries.First();
+        var htmlRenderer = new HtmlRenderer();
+        landingPageContent.ServiceIntroductionHtml = await htmlRenderer.ToHtml(landingPageContent.ServiceIntroduction);
+        return landingPageContent;
+    }
+}
