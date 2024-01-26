0c7d57a3bca8607844f268404670eb892c811ea7
Daniel Clarke <daniel.clarke234@gmail.com>
2024-01-26 09:37:49 +0000

    removed debugger attaching by default

--- src/Dfe.EarlyYearsQualification.Web/Program.cs
+++ src/Dfe.EarlyYearsQualification.Web/Program.cs
@@ -11,8 +11,6 @@ builder.Services.AddTransient<IContentService, ContentfulContentService>();
 
 var app = builder.Build();
 
-System.Diagnostics.Debugger.Launch();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
