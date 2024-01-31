2d2962ce8aedd4aff0fdb0293c2d05d87c0c8f89
Sam Carter <sam.carter@education.gov.uk>
2024-01-31 15:47:14 +0000

    Added network resources

--- terraform/azure-network/variables.tf
+++ terraform/azure-network/variables.tf
@@ -1,12 +1,15 @@
-variable "azure_region" {
-  default     = "westeurope"
+variable "environment" {
+  description = "Environment to deploy resources"
+  type        = string
+}
+
+variable "location" {
   description = "Name of the Azure region to deploy resources"
   type        = string
 }
 
-variable "environment" {
-  default     = "development"
-  description = "Environment to deploy resources"
+variable "resource_group" {
+  description = "Name of the Azure Resource Group to deploy resources"
   type        = string
 }
 
@@ -15,40 +18,44 @@ variable "resource_name_prefix" {
   type        = string
 }
 
-variable "admin_email_address" {
-  description = "Email Address of the Admin"
+variable "kv_certificate_authority_label" {
+  description = "Label for the Certificate Authority"
   type        = string
-  sensitive   = true
+}
+
+variable "kv_certificate_authority_name" {
+  description = "Name of the Certificate Authority"
+  type        = string
 }
 
 variable "kv_certificate_authority_username" {
   description = "Username for the Certificate provider"
   type        = string
-  sensitive   = true
 }
 
 variable "kv_certificate_authority_password" {
   description = "Password the Certificate provider"
   type        = string
-  sensitive   = true
+}
+
+variable "kv_certificate_authority_admin_email" {
+  description = "Email Address of the Certificate Authority Admin"
+  type        = string
 }
 
 variable "kv_certificate_authority_admin_first_name" {
   description = "First Name of the Certificate Authority Admin"
   type        = string
-  sensitive   = true
 }
 
 variable "kv_certificate_authority_admin_last_name" {
   description = "Last Name of the Certificate Authority Admin"
   type        = string
-  sensitive   = true
 }
 
 variable "kv_certificate_authority_admin_phone_no" {
   description = "Phone No. of the Certificate Authority Admin"
   type        = string
-  sensitive   = true
 }
 
 variable "kv_certificate_label" {
--- terraform/main.tf
+++ terraform/main.tf
@@ -24,3 +24,23 @@ resource "azurerm_resource_group" "rg" {
     ignore_changes = [tags]
   }
 }
+
+# Create Network Resources
+module "network" {
+  source = "./azure-network"
+
+  environment                               = var.environment
+  location                                  = var.azure_region
+  resource_group                            = azurerm_resource_group.rg.name
+  resource_name_prefix                      = var.resource_name_prefix
+  kv_certificate_authority_label            = "GlobalSignCA"
+  kv_certificate_authority_name             = "GlobalSign"
+  kv_certificate_authority_username         = var.kv_certificate_authority_username
+  kv_certificate_authority_password         = var.kv_certificate_authority_password
+  kv_certificate_authority_admin_email      = var.admin_email_address
+  kv_certificate_authority_admin_first_name = var.kv_certificate_authority_admin_first_name
+  kv_certificate_authority_admin_last_name  = var.kv_certificate_authority_admin_last_name
+  kv_certificate_authority_admin_phone_no   = var.kv_certificate_authority_admin_phone_no
+  kv_certificate_label                      = var.kv_certificate_label
+  kv_certificate_subject                    = var.kv_certificate_subject
+}
