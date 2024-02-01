9a4111df7ec6f009f30014e9c03ed9671de55f3f
Sam C <156680559+sam-c-dfe@users.noreply.github.com>
2024-02-01 14:41:25 +0000

    Initial commit of the terraform scripts (#8)
    * Initial commit of the terraform scripts to start creating the initial environment
    
    * Added the azure remote state scripts to store terraform state in blob storage
    
    * Added network resources
    
    * Added web app infrastructure scripts
    
    * Updated to add in temp values for the contentful key vault secrets
    
    * Updated secrets permission
    
    * Updated file names, removed some of the variables we don't need and changed a couple of minor things in the scripts
    
    * Removed app settings that is not needed
    
    * Removed concurrency setting as not needed
    
    * Updated the tags and added the new terraform.tf file

---
+++ terraform/local.tf
@@ -0,0 +1,30 @@
+locals {
+  # Common tags to be assigned to resources
+  common_tags = {
+    "Environment"      = var.environment
+    "Parent Business"  = "Children’s Care"
+    "Product"          = "Early Years Qualifications"
+    "Service Offering" = "Early Years Qualifications"
+  }
+
+  # Web Application Configuration
+  webapp_app_settings = {
+    "ENVIRONMENT"                         = var.environment
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "BOT_TOKEN"                           = var.webapp_config_bot_token
+    "DOMAIN"                              = var.webapp_config_domain
+    "FEEDBACK_URL"                        = var.webapp_config_feedback_url
+    "TRACKING_ID"                         = var.tracking_id
+    "WEBSITES_CONTAINER_START_TIME_LIMIT" = 720
+  }
+
+  webapp_slot_app_settings = {
+    "ENVIRONMENT"                         = var.environment
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "BOT_TOKEN"                           = var.webapp_config_bot_token
+    "DOMAIN"                              = var.webapp_config_domain
+    "FEEDBACK_URL"                        = var.webapp_config_feedback_url
+    "TRACKING_ID"                         = var.tracking_id
+    "WEBSITES_CONTAINER_START_TIME_LIMIT" = 720
+  }
+}
