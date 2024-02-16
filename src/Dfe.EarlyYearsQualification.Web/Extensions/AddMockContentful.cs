d5e24f7ac6333e90eb08258ac74118ac16d4044e
Sam Carter <sam.carter@education.gov.uk>
2024-02-16 09:31:04 +0000

    Updated contentful mock to add in navigation links

--- src/Dfe.EarlyYearsQualification.Web/Extensions/AddMockContentful.cs
+++ src/Dfe.EarlyYearsQualification.Web/Extensions/AddMockContentful.cs
@@ -1,4 +1,5 @@
 using Dfe.EarlyYearsQualification.Content.Services;
+using Dfe.EarlyYearsQualification.Content.Entities;
 using Moq;
 
 namespace Dfe.EarlyYearsQualification.Web.Extensions
@@ -10,11 +11,16 @@ namespace Dfe.EarlyYearsQualification.Web.Extensions
 
       var mockContentfulService = new Mock<IContentService>();
 
-      mockContentfulService.Setup(x => x.GetLandingPage()).ReturnsAsync(new Content.Entities.LandingPage()
+      mockContentfulService.Setup(x => x.GetLandingPage()).ReturnsAsync(new LandingPage()
       {
         Header = "Test Header",
         ServiceIntroduction = new Contentful.Core.Models.Document(),
         StartButtonText = "Test Start Button Text"
+      });
+
+      mockContentfulService.Setup(x => x.GetNavigationLinks()).ReturnsAsync(new List<NavigationLink>()
+      {
+        new NavigationLink() { DisplayText = "Privacy notice", Href="#"}
       });
 
       services.AddSingleton(mockContentfulService.Object);
