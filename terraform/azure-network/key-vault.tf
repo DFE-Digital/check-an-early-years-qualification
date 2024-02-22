948142c84324b63ecddb488a6e12f2f403be81e2
Sam C <156680559+sam-c-dfe@users.noreply.github.com>
2024-02-22 13:56:44 +0000

    EYQB-65 Updated the main build workflow to deploy out the docker imag… (#29)
    * EYQB-65 Updated the main build workflow to deploy out the docker image to the development environment
    
    * Updated to remove the secrets from KeyVault as no longer needed and updated the local to add the KeyVault Endpoint as an AppSetting
    
    * Fixed path for uploading / downloading the tfplan.

--- terraform/azure-network/key-vault.tf
+++ terraform/azure-network/key-vault.tf
@@ -1,6 +1,5 @@
 # Create Key Vault
 data "azurerm_client_config" "az_config" {}
-# data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv" {
   name                        = "${var.resource_name_prefix}-kv"
@@ -158,50 +157,3 @@ resource "azurerm_key_vault_certificate" "kv_cert" {
     }
   }
 }
-
-# resource "azurerm_key_vault_access_policy" "vault_access_policy_tf" {
-#   key_vault_id = azurerm_key_vault.kv.id
-#   tenant_id    = data.azurerm_client_config.current.tenant_id
-#   object_id    = data.azurerm_client_config.current.object_id
-# 
-#   secret_permissions = ["List", "Get", "Set"]
-# }
-
-# resource "azurerm_key_vault_secret" "vault_secret_contentful_deliveryapikey" {
-#   key_vault_id = azurerm_key_vault.kv.id
-#   name         = "ContentfulOptions--DeliveryApiKey"
-#   value        = "temp value"
-
-#   lifecycle {
-#     ignore_changes = [
-#       value,
-#       expiration_date
-#     ]
-#   }
-# }
-
-# resource "azurerm_key_vault_secret" "vault_secret_contentful_previewapikey" {
-#   key_vault_id = azurerm_key_vault.kv.id
-#   name         = "ContentfulOptions--PreviewApiKey"
-#   value        = "temp value"
-
-#   lifecycle {
-#     ignore_changes = [
-#       value,
-#       expiration_date
-#     ]
-#   }
-# }
-
-# resource "azurerm_key_vault_secret" "vault_secret_contentful_spaceid" {
-#   key_vault_id = azurerm_key_vault.kv.id
-#   name         = "ContentfulOptions--SpaceId"
-#   value        = "temp value"
-
-#   lifecycle {
-#     ignore_changes = [
-#       value,
-#       expiration_date
-#     ]
-#   }
-# }
--- terraform/local.tf
+++ terraform/local.tf
@@ -13,6 +13,7 @@ locals {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
     "TRACKING_ID"                         = var.tracking_id
     "WEBSITES_CONTAINER_START_TIME_LIMIT" = 720
+    "KeyVault:Endpoint"                   = "${var.resource_name_prefix}-kv.vault.azure.net/"
   }
 
   webapp_slot_app_settings = {
@@ -20,5 +21,6 @@ locals {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
     "TRACKING_ID"                         = var.tracking_id
     "WEBSITES_CONTAINER_START_TIME_LIMIT" = 720
+    "KeyVault:Endpoint"                   = "${var.resource_name_prefix}-kv.vault.azure.net/"
   }
 }
