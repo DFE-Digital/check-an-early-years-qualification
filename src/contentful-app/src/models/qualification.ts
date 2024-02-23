15d4a7e04700df3b256a86da6d2165c4f0509c7d
Daniel Clarke <daniel.clarke@education.gov.uk>
2024-02-23 15:08:28 +0000

    Added contentful app prototype

---
+++ src/contentful-app/src/models/qualification.ts
@@ -0,0 +1,9 @@
+export interface qualification {
+  qualificationId: number;
+  qualificationName: string;
+  qualificationLevel: string;
+  awardingOrganisationTitle: string;
+  fromWhichYear: string;
+  qualificationNumber: string;
+  notesAdditionalRequirements: string;
+}
