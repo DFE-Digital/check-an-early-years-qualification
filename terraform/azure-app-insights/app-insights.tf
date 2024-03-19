efbeb6b58a17d7df49af36988e104e70e17de240
Sam Carter <sam.carter@education.gov.uk>
2024-03-19 08:58:59 +0000

    Updated to add in the log analytics workspace

--- terraform/azure-app-insights/app-insights.tf
+++ terraform/azure-app-insights/app-insights.tf
@@ -1,6 +1,15 @@
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "${var.resource_name_prefix}-workspace"
+  resource_group_name = var.resource_group
+  location            = var.location
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
 resource "azurerm_application_insights" "main" {
   name                = "${var.resource_name_prefix}-appinsights"
   resource_group_name = var.resource_group
   location            = var.location
   application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.main.id
 }
