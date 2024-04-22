7d695f3f4d74435a5c50799121a9a37e04bed6c1
Sam C <156680559+sam-c-dfe@users.noreply.github.com>
2024-04-22 13:28:55 +0100

    Initial commit of the security header changes (#88)
    * Initial commit of the security header changes
    
    * Updated using statement
    
    * Added in new e2e test to prove back button functionality now we have the CSP header

--- tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/journey/journey-spec.cy.js
+++ tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/journey/journey-spec.cy.js
@@ -1,50 +1,67 @@
 describe('A spec used to test the various routes through the journey', () => {
-    beforeEach(() => {
-        cy.visit("/");
-        cy.get('.govuk-button--start').should('exist');
-    })
-    
-    // Mock details found in Dfe.EarlyYearsQualification.Web.Extensions.AddMockContentful. 
-    it("should redirect the user when they select qualification was awarded outside the UK", () => {
-      // home page
-      cy.get('.govuk-button--start').click();
+  beforeEach(() => {
+    cy.visit("/");
+    cy.get('.govuk-button--start').should('exist');
+  })
 
-      // where-was-the-qualification-awarded page
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/questions/where-was-the-qualification-awarded');
-      })
+  // Mock details found in Dfe.EarlyYearsQualification.Web.Extensions.AddMockContentful. 
+  it("should redirect the user when they select qualification was awarded outside the UK", () => {
+    // home page
+    cy.get('.govuk-button--start').click();
 
-      cy.get('#outside-uk').click();
-      cy.get('button[type="submit"]').click();
-
-      // qualification-outside-the-united-kingdom page
-      cy.location().should((loc) => {
-        expect(loc.pathname).to.eq('/advice/qualification-outside-the-united-kingdom');
-      })
+    // where-was-the-qualification-awarded page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/questions/where-was-the-qualification-awarded');
     })
 
-    it("should redirect the user when they select qualification was awarded in England", () => {
-        // home page
-        cy.get('.govuk-button--start').click();
-  
-        // where-was-the-qualification-awarded page
-        cy.location().should((loc) => {
-          expect(loc.pathname).to.eq('/questions/where-was-the-qualification-awarded');
-        })
-  
-        cy.get('#england').click();
-        cy.get('button[type="submit"]').click();
-  
-        // qualifications page (This is only a temporary page)
-        cy.location().should((loc) => {
-          expect(loc.pathname).to.eq('/qualifications');
-        })
+    cy.get('#outside-uk').click();
+    cy.get('button[type="submit"]').click();
 
-        cy.get('a[href="/qualifications/qualification-details/eyq-240"]').click();
+    // qualification-outside-the-united-kingdom page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/advice/qualification-outside-the-united-kingdom');
+    })
+  })
 
-        // qualifications page (This is only a temporary page)
-        cy.location().should((loc) => {
-            expect(loc.pathname).to.eq('/qualifications/qualification-details/eyq-240');
-        })
-      })
+  it("should redirect the user when they select qualification was awarded in England", () => {
+    // home page
+    cy.get('.govuk-button--start').click();
+
+    // where-was-the-qualification-awarded page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/questions/where-was-the-qualification-awarded');
+    })
+
+    cy.get('#england').click();
+    cy.get('button[type="submit"]').click();
+
+    // qualifications page (This is only a temporary page)
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/qualifications');
+    })
+
+    cy.get('a[href="/qualifications/qualification-details/eyq-240"]').click();
+
+    // qualifications page (This is only a temporary page)
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/qualifications/qualification-details/eyq-240');
+    })
+  })
+
+  it("should move the user back to the previous page when they click on the back button", () => {
+    // home page
+    cy.get('.govuk-button--start').click();
+
+    // where-was-the-qualification-awarded page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/questions/where-was-the-qualification-awarded');
+    })
+
+    cy.get('.govuk-back-link').click();
+
+    // home page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/');
+    })
+  })
 })
