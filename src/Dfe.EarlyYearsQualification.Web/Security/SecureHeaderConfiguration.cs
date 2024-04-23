08e76991d468289129a6c682078bb27d3e487e22
Sam Carter <sam.carter@education.gov.uk>
2024-04-23 09:59:25 +0100

    Updated to add frame ancestor for contentful

--- src/Dfe.EarlyYearsQualification.Web/Security/SecureHeaderConfiguration.cs
+++ src/Dfe.EarlyYearsQualification.Web/Security/SecureHeaderConfiguration.cs
@@ -26,8 +26,10 @@ public static class SecureHeaderConfiguration
         // This is to extend the ScriptSrc to allow the javascript inline code for the back button on the journey pages.
         var backButtonShaCspElement = new ContentSecurityPolicyElement { CommandType = CspCommandType.Directive, DirectiveOrUri = "sha256-2eCA8tPChvVMeSRvRNqlmBco1wRmAKXWVzJ8Vpb9S6Y=" };
         var unsafeHashesElement = new ContentSecurityPolicyElement { CommandType = CspCommandType.Directive, DirectiveOrUri = "unsafe-hashes" };
+        var contentfulCspElement = new ContentSecurityPolicyElement { CommandType = CspCommandType.Uri, DirectiveOrUri = "https://app.contentful.com" };
         configuration.ContentSecurityPolicyConfiguration.ScriptSrc.Add(backButtonShaCspElement);
         configuration.ContentSecurityPolicyConfiguration.ScriptSrc.Add(unsafeHashesElement);
+        configuration.ContentSecurityPolicyConfiguration.FrameAncestors.Add(contentfulCspElement);
         return configuration;
     }
 }
