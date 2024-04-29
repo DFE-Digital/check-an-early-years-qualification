36fd8adae87f244e3ebe9617142c8f3b36d55d80
Sam C <156680559+sam-c-dfe@users.noreply.github.com>
2024-04-29 14:46:54 +0100

    Updated to store anti forgery token keys in blob storage (#97)
    * Updated to store anti forgery token keys in blob storage
    
    * Minor tweak to program file
    
    * Moved the data protection section to resolve failing e2e tests
    
    * Moved the keyVaultEndpoint variable

---
+++ terraform/azure-storage/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "Name of the Azure region to deploy resources"
+  type        = string
+}
+
+variable "resource_group" {
+  description = "Name of the Azure Resource Group to deploy resources"
+  type        = string
+}
