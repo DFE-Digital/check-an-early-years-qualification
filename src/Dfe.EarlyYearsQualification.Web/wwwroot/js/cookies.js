fb0c2e4b893162e2c27f04aadfd18b658a026296
Daniel Clarke <daniel.clarke@education.gov.uk>
2024-04-30 16:51:46 +0100

    added cookie success banner

--- src/Dfe.EarlyYearsQualification.Web/wwwroot/js/cookies.js
+++ src/Dfe.EarlyYearsQualification.Web/wwwroot/js/cookies.js
@@ -1,3 +1,7 @@
 $("#cookies-button").click(function() {
   // TODO: write function to collect form data, and either clear cookies or do nothing
+
+  $("#cookies-set-banner").removeAttr('hidden');
+
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
