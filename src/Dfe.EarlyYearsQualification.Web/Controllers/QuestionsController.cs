39648098ba16139ef8e12bd71a41efbf4cf8df42
Sam C <156680559+sam-c-dfe@users.noreply.github.com>
2024-05-23 15:09:49 +0100

    EYQB-238: Initial commit of the changes to add in the When was the qualification started question page (#152)

--- src/Dfe.EarlyYearsQualification.Web/Controllers/QuestionsController.cs
+++ src/Dfe.EarlyYearsQualification.Web/Controllers/QuestionsController.cs
@@ -7,18 +7,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Dfe.EarlyYearsQualification.Web.Controllers;
 
-[Route("/questions/where-was-the-qualification-awarded")]
+[Route("/questions")]
 public class QuestionsController(ILogger<QuestionsController> logger, IContentService contentService)
     : Controller
 {
-    [HttpGet("")]
+    [HttpGet("where-was-the-qualification-awarded")]
     public async Task<IActionResult> WhereWasTheQualificationAwarded()
     {
         return await GetView(QuestionPages.WhereWasTheQualificationAwarded, "WhereWasTheQualificationAwarded",
                              "Questions");
     }
 
-    [HttpPost("")]
+    [HttpPost("where-was-the-qualification-awarded")]
     public async Task<IActionResult> WhereWasTheQualificationAwarded(QuestionModel model)
     {
         if (!ModelState.IsValid)
@@ -35,7 +35,29 @@ public class QuestionsController(ILogger<QuestionsController> logger, IContentSe
 
         return model.Option == Options.OutsideOfTheUnitedKingdom
                    ? RedirectToAction("QualificationOutsideTheUnitedKingdom", "Advice")
-                   : RedirectToAction("Get", "QualificationDetails");
+                   : RedirectToAction("WhenWasTheQualificationStarted");
+    }
+
+    [HttpGet("when-was-the-qualification-started")]
+    public IActionResult WhenWasTheQualificationStarted()
+    {
+        // TODO: This is just a temporary page until the design is finalised through UR
+        var questionPage = new QuestionPage()
+                           {
+                                CtaButtonText = "Continue",
+                                Question = "When was the qualification started?",
+                                Options = new List<Option>()
+                           };
+        var model = Map(new QuestionModel(), questionPage, "WhenWasTheQualificationStarted", "Questions");
+        return View("Question", model);
+    }
+
+    [HttpPost("when-was-the-qualification-started")]
+    public IActionResult WhenWasTheQualificationStarted(QuestionModel model)
+    {
+        // TODO: This is just a temporary page until the design is finalised through UR
+        // For now just redirect to the next page. Model validation will be done at a later date
+        return RedirectToAction("Get", "QualificationDetails");
     }
 
     private async Task<IActionResult> GetView(string questionPageId, string actionName, string controllerName)
