7c57f295cc707be414af5e9d25aa61a57fc7b405
Sam Carter <sam.carter@education.gov.uk>
2024-05-29 11:20:07 +0100

    Added in additional question e2e test for the new page and updated the pa11y config to pick up the new page

--- tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/pages/question-spec.cy.js
+++ tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/pages/question-spec.cy.js
@@ -30,4 +30,28 @@ describe("A spec that tests question pages", () => {
 
         cy.get("#question").should("contain.text", "When was the qualification started?");
     })
+
+    it("Checks the content on what-level-is-the-qualification page", () => {
+        cy.visit("/questions/what-level-is-the-qualification");
+
+        cy.get("#question").should("contain.text", "What level is the qualification?");
+        cy.get("#2").should("exist");
+        cy.get("#3").should("exist");
+    })
+
+    it("shows an error message when a user doesnt select an option on the what-level-is-the-qualification page", () => {
+        cy.visit("/questions/what-level-is-the-qualification");
+
+        cy.get("#option-error").should("not.exist");
+        cy.get(".govuk-form-group").should("not.have.class", "govuk-form-group--error");
+
+        cy.get('button[type="submit"]').click();
+        cy.location().should((loc) => {
+          expect(loc.pathname).to.eq("/questions/what-level-is-the-qualification");
+        })
+
+        cy.get('#option-error').should("exist");
+        cy.get('#option-error').should("contain.text", "Test error message");
+        cy.get(".govuk-form-group").should("have.class", "govuk-form-group--error");
+    })
 })
