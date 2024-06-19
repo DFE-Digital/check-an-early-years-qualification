486fbb30570f028885b03d4753ec7dca881e7fd0
Daniel Clarke <daniel.clarke@education.gov.uk>
2024-06-19 12:19:37 +0100

    Removed white space from user journey interface

--- src/Dfe.EarlyYearsQualification.Web/Services/UserJourneyCookieService/IUserJourneyCookieService.cs
+++ src/Dfe.EarlyYearsQualification.Web/Services/UserJourneyCookieService/IUserJourneyCookieService.cs
@@ -5,9 +5,7 @@ namespace Dfe.EarlyYearsQualification.Web.Services.UserJourneyCookieService;
 public interface IUserJourneyCookieService
 {
     public void SetWhereWasQualificationAwarded(string location);
-
     public void SetWhenWasQualificationAwarded(string date);
-
     public void SetLevelOfQualification(string level);
     public UserJourneyModel GetUserJourneyModelFromCookie();
     public void ResetUserJourneyCookie();
