f25d21ab94d3abce7d382054db7914ca8b73c0db
Daniel Clarke <daniel.clarke@education.gov.uk>
2024-06-20 17:01:42 +0100

    fixed dodgy main merge

--- src/Dfe.EarlyYearsQualification.Web/Security/SecureHeaderConfiguration.cs
+++ src/Dfe.EarlyYearsQualification.Web/Security/SecureHeaderConfiguration.cs
@@ -41,6 +41,24 @@ public static class SecureHeaderConfiguration
                                               DirectiveOrUri = "sha256-Om9RNNoMrdmIZzT4Oo7KaozVNUg6zYxVQuq3CPld2Ms="
                                           };
 
+        var govukFrontendSupportedElement = new ContentSecurityPolicyElement
+                                            {
+                                                CommandType = CspCommandType.Directive,
+                                                DirectiveOrUri = "sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw="
+                                            };
+        
+        var govukAllMinifiedElement = new ContentSecurityPolicyElement
+                                            {
+                                                CommandType = CspCommandType.Directive,
+                                                DirectiveOrUri = "sha256-l5MP+9OapFXGxjKMNj/89ExAW2TvAFFoADrbsmtSJXo="
+                                            };
+        
+        var dropdownPageCheckbox = new ContentSecurityPolicyElement
+                                   {
+                                       CommandType = CspCommandType.Directive,
+                                       DirectiveOrUri = "sha256-pwrEcsLN2o+4gQQDR/0sGCITSf0nhhLAzP4h73+5foc="
+                                   };
+
         var unsafeHashesElement = new ContentSecurityPolicyElement
                                   { CommandType = CspCommandType.Directive, DirectiveOrUri = "unsafe-hashes" };
         var contentfulCspElement = new ContentSecurityPolicyElement
@@ -49,7 +67,11 @@ public static class SecureHeaderConfiguration
         configuration.ContentSecurityPolicyConfiguration.ScriptSrc.Add(cookiesPageShaCspElement);
         configuration.ContentSecurityPolicyConfiguration.ScriptSrc.Add(windowLocationShaCspElement);
         configuration.ContentSecurityPolicyConfiguration.ScriptSrc.Add(unsafeHashesElement);
+        configuration.ContentSecurityPolicyConfiguration.ScriptSrc.Add(govukFrontendSupportedElement);
+        configuration.ContentSecurityPolicyConfiguration.ScriptSrc.Add(govukAllMinifiedElement);
         configuration.ContentSecurityPolicyConfiguration.FrameAncestors.Add(contentfulCspElement);
+        configuration.ContentSecurityPolicyConfiguration.ScriptSrc.Add(dropdownPageCheckbox);
+        
         return configuration;
     }
 }
