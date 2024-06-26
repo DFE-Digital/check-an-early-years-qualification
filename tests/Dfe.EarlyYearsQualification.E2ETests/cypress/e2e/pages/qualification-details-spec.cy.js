220008dd73d4654cecf86d24aa2a4726c05cdb19
Sam Carter <sam.carter@education.gov.uk>
2024-06-26 09:13:42 +0100

    Updated e2e test

--- tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/pages/qualification-details-spec.cy.js
+++ tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/pages/qualification-details-spec.cy.js
@@ -13,7 +13,6 @@ describe("A spec used to test the qualification details page", () => {
     cy.get("#qualification-level-value").should("contain.text", "3");
     cy.get("#qualification-number-value").should("contain.text", "603/5829/4");
     cy.get("#from-which-year-value").should("contain.text", "2020");
-    cy.get("#notes-value").should("contain.text", "The course must be assessed within the EYFS in an Early Years setting in England. Please note that the name of this qualification changed in February 2023. Qualifications achieved under either name are full and relevant provided that the start date for the qualification aligns with the date of the name change.");
     cy.get("#additional-requirements-value").should("contain.text", "Additional notes");
   })
 })
