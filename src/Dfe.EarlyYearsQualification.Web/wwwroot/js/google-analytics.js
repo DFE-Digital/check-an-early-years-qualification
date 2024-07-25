94db1e97cd0dce96673a9198ecf174835255db71
Sam Carter <sam.carter@education.gov.uk>
2024-07-25 11:50:30 +0100

    EYQB-469: Added GTM scripts

--- src/Dfe.EarlyYearsQualification.Web/wwwroot/js/google-analytics.js
+++
@@ -1,5 +0,0 @@
-let tag = document.getElementById('ga-script').getAttribute('data-ga-tag');
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', tag);
