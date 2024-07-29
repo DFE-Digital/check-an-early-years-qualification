87b9e31cd9d018a7dfb06bee9a04b19488aed62a
Sam Carter <sam.carter@education.gov.uk>
2024-07-29 13:43:06 +0100

    Added in additional test to cover a user who answered an additional requirement question incorrectly

--- tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/pages/qualification-details-spec.cy.js
+++ tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/pages/qualification-details-spec.cy.js
@@ -2,13 +2,12 @@ describe("A spec used to test the qualification details page", () => {
 
   beforeEach(() => {
     cy.setCookie('auth-secret', Cypress.env('auth_secret'));
-    // Value is '{"WhereWasQualificationAwarded":"england","WhenWasQualificationAwarded":"7/2015","LevelOfQualification":"3","WhatIsTheAwardingOrganisation":"NCFE","SearchCriteria":"","AdditionalQuestionsAnswers":{"Test question":"yes","Test question 2":"yes"}}' encoded
-    cy.setCookie('user_journey', '%7B%22WhereWasQualificationAwarded%22%3A%22england%22%2C%22WhenWasQualificationAwarded%22%3A%227%2F2015%22%2C%22LevelOfQualification%22%3A%223%22%2C%22WhatIsTheAwardingOrganisation%22%3A%22NCFE%22%2C%22SearchCriteria%22%3A%22%22%2C%22AdditionalQuestionsAnswers%22%3A%7B%22Test%20question%22%3A%22yes%22%2C%22Test%20question%202%22%3A%22yes%22%7D%7D');
-
   })
 
   // Mock details found in Dfe.EarlyYearsQualification.Mock.Content.MockContentfulService.  
   it("Checks the qualification details are on the page", () => {
+    // Value is '{"WhereWasQualificationAwarded":"england","WhenWasQualificationAwarded":"7/2015","LevelOfQualification":"3","WhatIsTheAwardingOrganisation":"NCFE","SearchCriteria":"","AdditionalQuestionsAnswers":{"Test question":"yes","Test question 2":"yes"}}' encoded
+    cy.setCookie('user_journey', '%7B%22WhereWasQualificationAwarded%22%3A%22england%22%2C%22WhenWasQualificationAwarded%22%3A%227%2F2015%22%2C%22LevelOfQualification%22%3A%223%22%2C%22WhatIsTheAwardingOrganisation%22%3A%22NCFE%22%2C%22SearchCriteria%22%3A%22%22%2C%22AdditionalQuestionsAnswers%22%3A%7B%22Test%20question%22%3A%22yes%22%2C%22Test%20question%202%22%3A%22yes%22%7D%7D');
     cy.visit("/qualifications/qualification-details/eyq-240");
 
     cy.get("#page-header").should("contain.text", "Test Main Heading");
@@ -33,6 +32,8 @@ describe("A spec used to test the qualification details page", () => {
   })
 
   it("Checks the order of the ratios on the page", () => {
+    // Value is '{"WhereWasQualificationAwarded":"england","WhenWasQualificationAwarded":"7/2015","LevelOfQualification":"3","WhatIsTheAwardingOrganisation":"NCFE","SearchCriteria":"","AdditionalQuestionsAnswers":{"Test question":"yes","Test question 2":"yes"}}' encoded
+    cy.setCookie('user_journey', '%7B%22WhereWasQualificationAwarded%22%3A%22england%22%2C%22WhenWasQualificationAwarded%22%3A%227%2F2015%22%2C%22LevelOfQualification%22%3A%223%22%2C%22WhatIsTheAwardingOrganisation%22%3A%22NCFE%22%2C%22SearchCriteria%22%3A%22%22%2C%22AdditionalQuestionsAnswers%22%3A%7B%22Test%20question%22%3A%22yes%22%2C%22Test%20question%202%22%3A%22yes%22%7D%7D');
     cy.visit("/qualifications/qualification-details/eyq-240");
 
     cy.get(".ratio-row").should('have.length', 4);
@@ -50,6 +51,29 @@ describe("A spec used to test the qualification details page", () => {
     cy.get(".govuk-tag").eq(1).should("have.class", "govuk-tag--green");
     cy.get(".govuk-tag").eq(2).should("have.class", "govuk-tag--green");
     cy.get(".govuk-tag").eq(3).should("have.class", "govuk-tag--green");
+    cy.get(".govuk-tag").eq(4).should("have.class", "govuk-tag--red");
+  })
+
+  it("Checks the order of the ratios on the page when a user incorrectly answers an additional requirement question", () => {
+    // Value is '{"WhereWasQualificationAwarded":"england","WhenWasQualificationAwarded":"7/2015","LevelOfQualification":"3","WhatIsTheAwardingOrganisation":"NCFE","SearchCriteria":"","AdditionalQuestionsAnswers":{"Test question":"yes","Test question 2":"no"}}' encoded
+    cy.setCookie('user_journey', '%7B%22WhereWasQualificationAwarded%22%3A%22england%22%2C%22WhenWasQualificationAwarded%22%3A%227%2F2015%22%2C%22LevelOfQualification%22%3A%223%22%2C%22WhatIsTheAwardingOrganisation%22%3A%22NCFE%22%2C%22SearchCriteria%22%3A%22%22%2C%22AdditionalQuestionsAnswers%22%3A%7B%22Test%20question%22%3A%22yes%22%2C%22Test%20question%202%22%3A%22no%22%7D%7D');
+    cy.visit("/qualifications/qualification-details/eyq-240");
+
+    cy.get(".ratio-row").should('have.length', 4);
+    cy.get(".ratio-heading").eq(0).should("contain.text", "Unqualified");
+    cy.get(".ratio-heading").eq(1).should("contain.text", "Level 6");
+    cy.get(".ratio-heading").eq(2).should("contain.text", "Level 3");
+    cy.get(".ratio-heading").eq(3).should("contain.text", "Level 2");
+
+    // Phase Banner uses govuk-tag also hence index starting at 1
+    cy.get(".govuk-tag").eq(1).should("contain.text", "Approved");
+    cy.get(".govuk-tag").eq(2).should("contain.text", "Not Approved");
+    cy.get(".govuk-tag").eq(3).should("contain.text", "Not Approved");
+    cy.get(".govuk-tag").eq(4).should("contain.text", "Not Approved");
+
+    cy.get(".govuk-tag").eq(1).should("have.class", "govuk-tag--green");
+    cy.get(".govuk-tag").eq(2).should("have.class", "govuk-tag--red");
+    cy.get(".govuk-tag").eq(3).should("have.class", "govuk-tag--red");
     cy.get(".govuk-tag").eq(4).should("have.class", "govuk-tag--red");
   })
 })
