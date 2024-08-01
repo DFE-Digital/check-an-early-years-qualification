6cdc27ef0df90e213303589c1386cf12bd32f1ef
Sam Carter <sam.carter@education.gov.uk>
2024-08-01 09:34:02 +0100

    Changed set to init

--- src/Dfe.EarlyYearsQualification.Web/Models/AdditionalInformationModel.cs
+++ src/Dfe.EarlyYearsQualification.Web/Models/AdditionalInformationModel.cs
@@ -2,7 +2,7 @@ namespace Dfe.EarlyYearsQualification.Web.Models;
 
 public class AdditionalInformationModel
 {
-    public string AdditionalInformationHeader { get; set; } = string.Empty;
+    public string AdditionalInformationHeader { get; init; } = string.Empty;
 
-    public string AdditionalInformationBody { get; set; } = string.Empty;
+    public string AdditionalInformationBody { get; init; } = string.Empty;
 }
