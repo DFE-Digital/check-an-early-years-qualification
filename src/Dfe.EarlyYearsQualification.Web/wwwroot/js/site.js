d9ab2e8c70077cbe15212bce305a7a07570bf30a
Sam Carter <sam.carter@education.gov.uk>
2024-08-05 16:41:39 +0100

    Updated to extend the print functionality so that the additional information is expanded on the preview and closes again once the preview closes.

--- src/Dfe.EarlyYearsQualification.Web/wwwroot/js/site.js
+++ src/Dfe.EarlyYearsQualification.Web/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function printButtonClicked()
+{
+    console.log('button clicked');
+    $('.govuk-details').attr('open', 'open');
+    window.print();
+}
+
+window.addEventListener("afterprint", (event) => {
+    console.log('after print');
+    $('.govuk-details').removeAttr('open');
+})
