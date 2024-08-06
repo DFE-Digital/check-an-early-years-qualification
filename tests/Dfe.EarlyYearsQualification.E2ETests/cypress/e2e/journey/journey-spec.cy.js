6bed1bc7d4ef33aafb370071f68cf007f672e7cf
RobertGHippo <robert.gammon@education.gov.uk>
2024-08-06 13:56:36 +0100

    Fix parenthesis mismatch.

--- tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/journey/journey-spec.cy.js
+++ tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/journey/journey-spec.cy.js
@@ -105,15 +105,15 @@ describe('A spec used to test the various routes through the journey', () => {
     })
     cy.get('#3').click();
     cy.get('button[id="question-submit"]').click();
-    
+
     // what-is-the-awarding-organisation page
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq('/questions/what-is-the-awarding-organisation');
     })
-    
+
     cy.get('#awarding-organisation-select').select(1);  // first no-default item in the list
     cy.get('button[id="question-submit"]').click();
-    
+
     // qualifications page
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq('/qualifications');
@@ -137,7 +137,7 @@ describe('A spec used to test the various routes through the journey', () => {
     cy.get('#yes_0_0').click();
     cy.get('#yes_1_0').click();
     cy.get('button[id="additional-requirement-button"]').click();
-    
+
     // qualification details page
     cy.location().should((loc) => {
       expect(loc.pathname).to.eq('/qualifications/qualification-details/EYQ-240');
@@ -225,72 +225,73 @@ describe('A spec used to test the various routes through the journey', () => {
       cy.location().should((loc) => {
         expect(loc.pathname).to.eq('/advice/qualification-level-7');
       })
-    
-    // check back button goes back to the what level is the qualification page
-    cy.get('#back-button').click();
 
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq('questions/what-level-is-the-qualification');
-    })
-  })
+      // check back button goes back to the what level is the qualification page
+      cy.get('#back-button').click();
 
-  it("should move the user back to the previous page when they click on the back button", () => {
-    // home page
-    cy.get('.govuk-button--start').click();
-
-    // where-was-the-qualification-awarded page
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq('/questions/where-was-the-qualification-awarded');
+      cy.location().should((loc) => {
+        expect(loc.pathname).to.eq('questions/what-level-is-the-qualification');
+      })
     })
 
-    cy.get('.govuk-back-link').click();
+    it("should move the user back to the previous page when they click on the back button", () => {
+      // home page
+      cy.get('.govuk-button--start').click();
 
-    // home page
-    cy.location().should((loc) => {
-      expect(loc.pathname).to.eq('/');
+      // where-was-the-qualification-awarded page
+      cy.location().should((loc) => {
+        expect(loc.pathname).to.eq('/questions/where-was-the-qualification-awarded');
+      })
+
+      cy.get('.govuk-back-link').click();
+
+      // home page
+      cy.location().should((loc) => {
+        expect(loc.pathname).to.eq('/');
+      })
     })
-  })
 
-  const testDates = [
+    const testDates = [
       ['09', '2014'],
       ['06', '2017'],
       ['08', '2019'],
-  ];
-  
-  testDates.forEach((date) => {
-        const [month, year] = date;
-        it(`should redirect when qualification is level 2 and startMonth is ${month} and startYear is ${year}`, () => {
-          // home page
-          cy.get('.govuk-button--start').click();
-    
-          // where-was-the-qualification-awarded page
-          cy.location().should((loc) => {
-            expect(loc.pathname).to.eq('/questions/where-was-the-qualification-awarded');
-          })
-    
-          cy.get('#england').click();
-          cy.get('button[id="question-submit"]').click();
-    
-          // when-was-the-qualification-started page
-          cy.location().should((loc) => {
-            expect(loc.pathname).to.eq('/questions/when-was-the-qualification-started');
-          })
-    
-          cy.get('#date-started-month').type(month);
-          cy.get('#date-started-year').type(year);
-          cy.get('button[id="question-submit"]').click();
-    
-          // what-level-is-the-qualification page
-          cy.location().should((loc) => {
-            expect(loc.pathname).to.eq('/questions/what-level-is-the-qualification');
-          })
-          cy.get('#2').click();
-          cy.get('button[id="question-submit"]').click();
-    
-          // level-2-qualifications-started-between-1-sept-2014-and-31-aug-2019 page
-          cy.location().should((loc) => {
-            expect(loc.pathname).to.eq('/advice/level-2-qualifications-started-between-1-sept-2014-and-31-aug-2019');
-          })
+    ];
+
+    testDates.forEach((date) => {
+      const [month, year] = date;
+      it(`should redirect when qualification is level 2 and startMonth is ${month} and startYear is ${year}`, () => {
+        // home page
+        cy.get('.govuk-button--start').click();
+
+        // where-was-the-qualification-awarded page
+        cy.location().should((loc) => {
+          expect(loc.pathname).to.eq('/questions/where-was-the-qualification-awarded');
+        })
+
+        cy.get('#england').click();
+        cy.get('button[id="question-submit"]').click();
+
+        // when-was-the-qualification-started page
+        cy.location().should((loc) => {
+          expect(loc.pathname).to.eq('/questions/when-was-the-qualification-started');
+        })
+
+        cy.get('#date-started-month').type(month);
+        cy.get('#date-started-year').type(year);
+        cy.get('button[id="question-submit"]').click();
+
+        // what-level-is-the-qualification page
+        cy.location().should((loc) => {
+          expect(loc.pathname).to.eq('/questions/what-level-is-the-qualification');
+        })
+        cy.get('#2').click();
+        cy.get('button[id="question-submit"]').click();
+
+        // level-2-qualifications-started-between-1-sept-2014-and-31-aug-2019 page
+        cy.location().should((loc) => {
+          expect(loc.pathname).to.eq('/advice/level-2-qualifications-started-between-1-sept-2014-and-31-aug-2019');
+        })
       })
+    })
   })
 })
