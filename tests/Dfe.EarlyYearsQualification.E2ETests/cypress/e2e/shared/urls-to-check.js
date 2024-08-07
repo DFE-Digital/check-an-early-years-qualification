fa86bdb09f91301a8a82c76bc11c6a1bdd90d474
Sam Carter <sam.carter@education.gov.uk>
2024-08-07 09:09:06 +0100

    Updated the urls to check

--- tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/shared/urls-to-check.js
+++ tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/shared/urls-to-check.js
@@ -1,8 +1,6 @@
 export const pages = [
   "/",
   "/accessibility-statement",
-  "/advice/qualification-outside-the-united-kingdom",
-  "/advice/level-2-qualifications-started-between-1-sept-2014-and-31-aug-2019",
   "/cookies",
   "/questions/where-was-the-qualification-awarded",
   "/questions/when-was-the-qualification-started",
@@ -12,10 +10,13 @@ export const pages = [
   "/confirm-qualification/EYQ-240",
   "/qualifications/qualification-details/EYQ-240",
   "/qualifications/check-additional-questions/EYQ-240",
+  "/advice/qualification-outside-the-united-kingdom",
+  "/advice/level-2-qualifications-started-between-1-sept-2014-and-31-aug-2019",
   "/advice/qualifications-achieved-in-scotland",
   "/advice/qualifications-achieved-in-wales",
   "/advice/qualifications-achieved-in-northern-ireland",
   "/advice/qualification-not-on-the-list",
+  "/advice/qualification-level-7",
   "/advice/level-6-qualification-pre-2014",
   "/advice/level-6-qualification-post-2014"
 ];
@@ -34,13 +35,14 @@ export const pagesWithForms = [
 export const pagesWithoutForms = [
   "/",
   "/accessibility-statement",
+  "/qualifications/qualification-details/EYQ-240",
   "/advice/qualification-outside-the-united-kingdom",
   "/advice/level-2-qualifications-started-between-1-sept-2014-and-31-aug-2019",
-  "/qualifications/qualification-details/EYQ-240",
   "/advice/qualifications-achieved-in-scotland",
   "/advice/qualifications-achieved-in-wales",
   "/advice/qualifications-achieved-in-northern-ireland",
   "/advice/qualification-not-on-the-list",
+  "/advice/qualification-level-7",
   "/advice/level-6-qualification-pre-2014",
   "/advice/level-6-qualification-post-2014"
 ]
