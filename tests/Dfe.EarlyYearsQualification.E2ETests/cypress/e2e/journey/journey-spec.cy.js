0de4b47dabe3f56c85a0f49de30a70bb172103cc
Sam Carter <sam.carter@education.gov.uk>
2024-08-08 16:27:57 +0100

    Added additional e2e test

--- tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/journey/journey-spec.cy.js
+++ tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/journey/journey-spec.cy.js
@@ -343,6 +343,71 @@ describe('A spec used to test the various routes through the journey', () => {
     })
   })
 
+  it("Should remove the search criteria when a user goes to the awarding organisation page and back again", () => {
+    // home page
+    cy.get('.govuk-button--start').click();
+
+    // where-was-the-qualification-awarded page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/questions/where-was-the-qualification-awarded');
+    })
+
+    cy.get('#england').click();
+    cy.get('button[id="question-submit"]').click();
+
+    // when-was-the-qualification-started page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/questions/when-was-the-qualification-started');
+    })
+
+    cy.get('#date-started-month').type("6");
+    cy.get('#date-started-year').type("2022");
+    cy.get('button[id="question-submit"]').click();
+
+    // what-level-is-the-qualification page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/questions/what-level-is-the-qualification');
+    })
+    cy.get('#3').click();
+    cy.get('button[id="question-submit"]').click();
+
+    // what-is-the-awarding-organisation page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/questions/what-is-the-awarding-organisation');
+    })
+
+    cy.get('#awarding-organisation-select').select(1);  // first no-default item in the list
+    cy.get('button[id="question-submit"]').click();
+
+    // Select a qualification page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/qualifications');
+    })
+  
+    cy.get('#refineSearch').type('test');
+    cy.get('#refineSearchButton').click();
+
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/qualifications');
+    })
+    
+    cy.get('#back-button').click();
+    // what-is-the-awarding-organisation page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/questions/what-is-the-awarding-organisation');
+    })
+
+    cy.get('#awarding-organisation-select').select(1);  // first no-default item in the list
+    cy.get('button[id="question-submit"]').click();
+
+    // Select a qualification page
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/qualifications');
+    })
+
+    cy.get('#refineSearch').should('have.value', '');
+  })
+
   const testDates = [
     ['09', '2014'],
     ['06', '2017'],
