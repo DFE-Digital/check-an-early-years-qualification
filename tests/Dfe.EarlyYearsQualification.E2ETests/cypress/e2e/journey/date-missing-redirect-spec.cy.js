0ce454c7c88bc6a71f7fdc023e4aad88def978dc
Daniel Clarke <daniel.clarke@education.gov.uk>
2024-08-21 12:29:26 +0100

    added auth cookie to redirect check test

--- tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/journey/date-missing-redirect-spec.cy.js
+++ tests/Dfe.EarlyYearsQualification.E2ETests/cypress/e2e/journey/date-missing-redirect-spec.cy.js
@@ -3,6 +3,8 @@ import { pagesThatRedirectIfDateMissing } from "../shared/urls-to-check";
 describe('A spec used to check that if the user skips entering the date of the qual, then they are redirected back to the date selection page', () => {
     pagesThatRedirectIfDateMissing.forEach((page) => {
         it(`navigating to ${page} should redirect the user to the date selection page`, () => {
+
+            cy.setCookie('auth-secret', Cypress.env('auth_secret'));
             
             cy.visit(page);
 
