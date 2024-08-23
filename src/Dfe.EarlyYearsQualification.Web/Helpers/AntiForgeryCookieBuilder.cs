9dff3ec7f77a3bbc3e9ee58b1558eab85fc15354
Sam Carter <sam.carter@education.gov.uk>
2024-08-23 11:08:04 +0100

    EYQB-516: Added new cookie builder to always set the secure flag on anti forgery token

---
+++ src/Dfe.EarlyYearsQualification.Web/Helpers/AntiForgeryCookieBuilder.cs
@@ -0,0 +1,14 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Dfe.EarlyYearsQualification.Web.Helpers;
+
+[ExcludeFromCodeCoverage]
+public class AntiForgeryCookieBuilder : CookieBuilder
+{
+    public override CookieOptions Build(HttpContext context, DateTimeOffset expiresFrom)
+    {
+        var cookieOptions = base.Build(context, expiresFrom);
+        cookieOptions.Secure = true;
+        return cookieOptions;
+    }
+}
--- src/Dfe.EarlyYearsQualification.Web/Program.cs
+++ src/Dfe.EarlyYearsQualification.Web/Program.cs
@@ -36,10 +36,18 @@ if (!useMockContentful)
 }
 
 // Add services to the container.
-builder.Services.AddAntiforgery(options => options.Cookie.SecurePolicy =
-                                               builder.Environment.IsDevelopment() || useMockContentful
-                                                   ? CookieSecurePolicy.SameAsRequest
-                                                   : CookieSecurePolicy.Always);
+builder.Services.AddAntiforgery(options =>
+                                {
+                                    options.Cookie = new AntiForgeryCookieBuilder
+                                                     {
+                                                         Name = ".AspNetCore.Antiforgery",
+                                                         SameSite = SameSiteMode.Strict,
+                                                         HttpOnly = true,
+                                                         IsEssential = true,
+                                                         SecurePolicy = CookieSecurePolicy.None
+                                                     };
+                                });
+
 builder.Services.AddControllersWithViews(options =>
                                          {
                                              // Ensures that all POST actions are protected by default.
