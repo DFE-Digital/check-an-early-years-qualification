6a2dd9c09dec7eb882e6aaeada5bce3f1d4e86f0
Sam C <156680559+sam-c-dfe@users.noreply.github.com>
2024-08-23 08:39:28 +0100

    EYQB-516: Changed logic to set to Always once deployed (#326)

--- src/Dfe.EarlyYearsQualification.Web/Program.cs
+++ src/Dfe.EarlyYearsQualification.Web/Program.cs
@@ -36,7 +36,10 @@ if (!useMockContentful)
 }
 
 // Add services to the container.
-builder.Services.AddAntiforgery(options => options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest);
+builder.Services.AddAntiforgery(options => options.Cookie.SecurePolicy =
+                                               builder.Environment.IsDevelopment() || useMockContentful
+                                                   ? CookieSecurePolicy.SameAsRequest
+                                                   : CookieSecurePolicy.Always);
 builder.Services.AddControllersWithViews(options =>
                                          {
                                              // Ensures that all POST actions are protected by default.
@@ -100,7 +103,7 @@ if (!app.Environment.IsDevelopment() || useMockContentful)
 {
     app.UseExceptionHandler("/Error");
     app.UseStatusCodePagesWithReExecute("/Error/{0}");
-    
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
