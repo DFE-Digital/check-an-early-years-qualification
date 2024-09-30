6edacdf30b08b8b93bc0f62aa1356145b1b0b3d5
Sam Carter <sam.carter@education.gov.uk>
2024-09-30 11:33:21 +0100

    Added unit test for the mock

--- tests/Dfe.EarlyYearsQualification.UnitTests/Mocks/MockContentfulServiceTests.cs
+++ tests/Dfe.EarlyYearsQualification.UnitTests/Mocks/MockContentfulServiceTests.cs
@@ -136,6 +136,19 @@ public class MockContentfulServiceTests
         result.Body!.Content[0].Should().BeAssignableTo<Paragraph>()
               .Which.Content.Should().ContainSingle(x => ((Text)x).Value == "Test Advice Page Body");
     }
+    
+    [TestMethod]
+    public async Task GetAdvicePage_privacyPolicy_ReturnsExpectedDetails()
+    {
+        var contentfulService = new MockContentfulService();
+
+        var result = await contentfulService.GetAdvicePage(AdvicePages.TemporaryPrivacyPolicy);
+        result.Should().NotBeNull();
+        result.Should().BeAssignableTo<AdvicePage>();
+        result!.Heading.Should().NotBeNullOrEmpty();
+        result.Body!.Content[0].Should().BeAssignableTo<Paragraph>()
+              .Which.Content.Should().ContainSingle(x => ((Text)x).Value == "Test Advice Page Body");
+    }
 
     [TestMethod]
     public async Task GetAdvicePage_UnknownEntryId_ReturnsException()
