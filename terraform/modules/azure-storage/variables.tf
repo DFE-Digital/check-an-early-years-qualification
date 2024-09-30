e5757d008cd9725b47a776f41c08ccc969a1e57d
RobertGHippo <robert.gammon@education.gov.uk>
2024-09-30 13:15:37 +0100

    Add network rule for storage account

--- terraform/modules/azure-storage/variables.tf
+++ terraform/modules/azure-storage/variables.tf
@@ -17,3 +17,8 @@ variable "tags" {
   description = "Resource tags"
   type        = map(string)
 }
+
+variable "webapp_subnet_id" {
+  description = "The ID of the subnet"
+  type        = string
+}
