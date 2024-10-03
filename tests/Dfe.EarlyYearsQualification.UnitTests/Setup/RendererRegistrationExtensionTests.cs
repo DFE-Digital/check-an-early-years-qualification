da00ac426664fe28334c1c4f57bf687db19acf0f
RobertGHippo <robert.gammon@education.gov.uk>
2024-10-03 15:44:45 +0100

    Reduce this method's complexity below the threshold.

--- tests/Dfe.EarlyYearsQualification.UnitTests/Setup/RendererRegistrationExtensionTests.cs
+++ tests/Dfe.EarlyYearsQualification.UnitTests/Setup/RendererRegistrationExtensionTests.cs
@@ -23,105 +23,61 @@ public class RendererRegistrationExtensionTests
 
         services.Count.Should().Be(20);
 
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(PhaseBannerRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(ParagraphRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(ExternalNavigationLinkRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(Heading1Renderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(Heading2Renderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(Heading3Renderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(Heading4Renderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(Heading5Renderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(Heading6Renderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(HyperlinkRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(InsetTextRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(MailtoLinkRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(SuccessBannerRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(TableCellRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(TableHeadingRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(TableRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(TableRowRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(UnorderedListRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IContentRenderer)
-                                             && s.ImplementationInstance != null
-                                             && s.ImplementationInstance.GetType() == typeof(UnorderedListHyperlinksRenderer)
-                                             && s.Lifetime == ServiceLifetime.Singleton);
-        
-        services.Should().ContainSingle(s => s.ServiceType == typeof(IGovUkContentParser)
-                                       && s.ImplementationType == typeof(GovUkContentParser)
-                                       && s.Lifetime == ServiceLifetime.Transient);
+        VerifyService<IContentRenderer, PhaseBannerRenderer>(services, ServiceLifetime.Singleton);
 
-        
+        VerifyService<IContentRenderer, ParagraphRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, ExternalNavigationLinkRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, Heading1Renderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, Heading2Renderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, Heading3Renderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, Heading4Renderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, Heading5Renderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, Heading6Renderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, HyperlinkRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, InsetTextRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, MailtoLinkRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, SuccessBannerRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, TableCellRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, TableHeadingRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, TableRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, TableRowRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, UnorderedListRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IContentRenderer, UnorderedListHyperlinksRenderer>(services, ServiceLifetime.Singleton);
+
+        VerifyService<IGovUkContentParser, GovUkContentParser>(services, ServiceLifetime.Transient, AllowNulls.Yes);
+    }
+
+    private static void VerifyService<TService, TInstance>(ServiceCollection services, ServiceLifetime lifetime,
+                                                           AllowNulls allowNulls = AllowNulls.No)
+    {
+        services.Should()
+                .ContainSingle(s => s.ServiceType == typeof(TService)
+                                    && (allowNulls == AllowNulls.Yes
+                                        || (s.ImplementationInstance != null &&
+                                            s.ImplementationInstance.GetType() == typeof(TInstance)))
+                                    && s.Lifetime == lifetime);
+    }
+
+    private enum AllowNulls
+    {
+        No,
+        Yes
     }
 }
