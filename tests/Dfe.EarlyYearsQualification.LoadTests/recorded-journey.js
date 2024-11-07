e483d51254f6a71b325f6858911ee33e40f17e20
RobertGHippo <robert.gammon@education.gov.uk>
2024-11-07 12:10:54 +0000

    Added "start page" group. Handle antiforgery.

--- tests/Dfe.EarlyYearsQualification.LoadTests/recorded-journey.js
+++ tests/Dfe.EarlyYearsQualification.LoadTests/recorded-journey.js
@@ -1,38 +1,228 @@
-// Adapted from recording created by Grafana k6 Browser Recorder 1.0.4
+// Adapted from recordings created by Grafana k6 Browser Recorder 1.0.4
 
 import { sleep, group, check } from 'k6'
 import http from 'k6/http'
 
 export const options = {
   stages: [
-    { duration: '3m', target: 10 },
-    { duration: '5m', target: 10 },
-    { duration: '10m', target: 50 },
+    { duration: '10m', target: 100 },
+    { duration: '5m', target: 100 },
+    { duration: '10m', target: 300 },
+    { duration: '5m', target: 300 },
+    { duration: '5m', target: 20 },
     { duration: '3m', target: 0 }
-  ],
-  challenge: __ENV.CHALLENGE_PASSWORD, // secret value, passed in from the CLI
-  customDomain: __ENV.CUSTOM_DOMAIN // custom domain, the address of the service to test, passed in from the CLI
-
-
+  ]
 }
+
+const password = __ENV.CHALLENGE_PASSWORD; // secret value, passed in from the CLI
+const customDomain = __ENV.CUSTOM_DOMAIN; // custom domain, the address of the service to test, passed in from the CLI
 
 export default function main() {
   let response
 
   const cookieJar = http.cookieJar();
-  const address = 'https://' + options.customDomain + '/';
+  const address = 'https://' + customDomain + '/';
 
-  cookieJar.set(address, 'auth-secret', options.challenge);
+  cookieJar.set(address, 'auth-secret', password);
 
-  // Page 1: start
-  response = http.get(address);
+  let antiForgeryToken;
+  let requestVerificationToken;
 
-  const cookies = cookieJar.cookiesForURL(address);
+  group(
+    `page_1 - ${address}`,
+    function () {
+      response = http.get('https://staging.check-an-early-years-qualification.education.gov.uk/', {
+        headers: {
+          'upgrade-insecure-requests': '1',
+          'user-agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+          'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+        },
+      })
 
-  check(response, {
-    "has cookie 'auth-secret'": (r) => cookies['auth-secret'].length > 0,
-    'cookie has expected value': (r) => cookies['auth-secret'][0] === options.challenge
-  })
+      const cookies = cookieJar.cookiesForURL(address);
+
+      antiForgeryToken = cookies['.AspNetCore.Antiforgery'][0];
+
+      requestVerificationToken = getRequestVerificationTokenValue(response);
+
+      check(response, {
+        "has cookie 'auth-secret'": (r) => cookies['auth-secret'].length > 0,
+        "'auth-secret' cookie has expected value": (r) => cookies['auth-secret'][0] === password,
+        'anti-forgery token has value': (r) => antiForgeryToken && antiForgeryToken.length > 0
+      });
+
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/lib/bootstrap/dist/css/bootstrap.min.css',
+        {
+          headers: {
+            referer: '',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/govuk/all.min.css?v=GgJd433hcU0_9bAVW6i2iBx3ytWDyVLKH-9vfxG4UxI',
+        {
+          headers: {
+            referer: '',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/css/site.css?v=igoQaqnM1Mg8CnrQJH6phEDWpG58m_dbMUgZxK0EsYY',
+        {
+          headers: {
+            referer: '',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/lib/jquery/dist/jquery.min.js',
+        {
+          headers: {
+            referer: '',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/lib/bootstrap/dist/js/bootstrap.bundle.min.js',
+        {
+          headers: {
+            referer: '',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/js/site.js?v=PVhEInckNOQLnX1CbKZYN9JbKFv7pI2XJKwv1s_zsGY',
+        {
+          headers: {
+            referer: '',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/govuk/all.min.js?v=qWAac5Qu0FixAEn44iyug6gG9I_RILPlCYO7ZpK9z_0',
+        {
+          headers: {
+            origin: 'https://staging.check-an-early-years-qualification.education.gov.uk',
+            referer: '',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/assets/fonts/light-94a07e06a1-v2.woff2',
+        {
+          headers: {
+            origin: 'https://staging.check-an-early-years-qualification.education.gov.uk',
+            referer:
+              'https://staging.check-an-early-years-qualification.education.gov.uk/govuk/all.min.css?v=GgJd433hcU0_9bAVW6i2iBx3ytWDyVLKH-9vfxG4UxI',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/assets/images/govuk-crest.png',
+        {
+          headers: {
+            referer:
+              'https://staging.check-an-early-years-qualification.education.gov.uk/govuk/all.min.css?v=GgJd433hcU0_9bAVW6i2iBx3ytWDyVLKH-9vfxG4UxI',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/assets/fonts/bold-b542beb274-v2.woff2',
+        {
+          headers: {
+            origin: 'https://staging.check-an-early-years-qualification.education.gov.uk',
+            referer:
+              'https://staging.check-an-early-years-qualification.education.gov.uk/govuk/all.min.css?v=GgJd433hcU0_9bAVW6i2iBx3ytWDyVLKH-9vfxG4UxI',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/govuk/all.min.js',
+        {
+          headers: {
+            origin: 'https://staging.check-an-early-years-qualification.education.gov.uk',
+            referer: '',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+      response = http.get(
+        'https://staging.check-an-early-years-qualification.education.gov.uk/assets/images/favicon.svg',
+        {
+          headers: {
+            referer: '',
+            'user-agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+            'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+          },
+        }
+      )
+    }
+    // function () {
+
+    //   response = http.get(address);
+
+  )
 
   group(
     `page_2 - ${address}questions/start-new`,
@@ -50,6 +240,12 @@ export default function main() {
           },
         }
       )
+
+      requestVerificationToken = getRequestVerificationTokenValue(response);
+
+      check(response, {
+        'request-verification token has value': (r) => requestVerificationToken && requestVerificationToken.length > 0
+      })
 
       response = http.get(
         `${address}/lib/bootstrap/dist/css/bootstrap.min.css`,
@@ -230,8 +426,7 @@ export default function main() {
         `${address}/questions/where-was-the-qualification-awarded`,
         {
           Option: 'england',
-          __RequestVerificationToken:
-            'CfDJ8AK9tTBzYtFDpZlgo1wVLPwmPItT5fmSdmtSvIXn2AMCppjGdkuTSebPG3ioheos3EyvalJv88CV10gfdVuEvvco40yDkS1O7Ki_jnlwFPo9K8H_bYgIdTrMuTVXaDZzGkAjMIer78sBc8alLMM5Ids',
+          __RequestVerificationToken: requestVerificationToken,
         },
         {
           headers: {
@@ -246,6 +441,8 @@ export default function main() {
           },
         }
       )
+
+      requestVerificationToken = getRequestVerificationTokenValue(response);
 
       response = http.get(
         `${address}/lib/bootstrap/dist/css/bootstrap.min.css`,
@@ -413,8 +610,7 @@ export default function main() {
         {
           SelectedMonth: '9',
           SelectedYear: '2014',
-          __RequestVerificationToken:
-            'CfDJ8AK9tTBzYtFDpZlgo1wVLPy_sGrFFGMITAgLReUpVjsAllHsg1WmvST2YIWra8VLR-R8zK9tLwifWUxTVBrnTHFpGZ2n-t1pdc5yGewMYE6V1x-rqKPqV_bqX_Mm-eEK64nwAIB4TRRaVQUazKTKios',
+          __RequestVerificationToken: requestVerificationToken,
         },
         {
           headers: {
@@ -429,6 +625,8 @@ export default function main() {
           },
         }
       )
+
+      requestVerificationToken = getRequestVerificationTokenValue(response);
 
       response = http.get(
         `${address}/lib/bootstrap/dist/css/bootstrap.min.css`,
@@ -595,8 +793,7 @@ export default function main() {
         `${address}/questions/what-level-is-the-qualification`,
         {
           Option: '3',
-          __RequestVerificationToken:
-            'CfDJ8AK9tTBzYtFDpZlgo1wVLPzTrY2tAkeWXoNbDMRGW0vqrzc8DOVIt69TZP6q_QiLuObScGkAdwc61oARY0slSa55hT2VRPIMVIvqwhR4Uqzz-jmJHDqI7OeDFzvQenjkNzVNF8SpQfzlJZ9A5aAS-jo',
+          __RequestVerificationToken: requestVerificationToken,
         },
         {
           headers: {
@@ -611,6 +808,8 @@ export default function main() {
           },
         }
       )
+
+      requestVerificationToken = getRequestVerificationTokenValue(response);
 
       response = http.get(
         `${address}/lib/bootstrap/dist/css/bootstrap.min.css`,
@@ -777,8 +976,7 @@ export default function main() {
         `${address}/questions/what-is-the-awarding-organisation`,
         {
           SelectedValue: 'NCFE',
-          __RequestVerificationToken:
-            'CfDJ8AK9tTBzYtFDpZlgo1wVLPx28l7L8gyTAdWeKgeabgYp6ME-sSXKHuC_dZ1tXaUXVn1LNx-IXX2mWM4LaNcmsP62meeZrJnkqE1fnJduP5WKN-ASSJexlSuZXJOaWyaJFHJ3RYx8CKxl8JPynrgnh50',
+          __RequestVerificationToken: requestVerificationToken,
           NotInTheList: 'false',
         },
         {
@@ -794,6 +992,8 @@ export default function main() {
           },
         }
       )
+
+      requestVerificationToken = getRequestVerificationTokenValue(response);
 
       response = http.get(
         `${address}/lib/bootstrap/dist/css/bootstrap.min.css`,
@@ -974,6 +1174,9 @@ export default function main() {
           },
         }
       )
+
+      requestVerificationToken = getRequestVerificationTokenValue(response);
+
       response = http.get(
         `${address}/lib/bootstrap/dist/css/bootstrap.min.css`,
         {
@@ -1135,8 +1338,7 @@ export default function main() {
         {
           qualificationId: 'EYQ-224',
           ConfirmQualificationAnswer: 'yes',
-          __RequestVerificationToken:
-            'CfDJ8AK9tTBzYtFDpZlgo1wVLPxg4rfJucdN41aAUvN1qkPPojHKgT0ofJxkRm4IEjDTHW2GnMfpXPvKwDB5qjcyWdTgneyDGKkBNO9_g3EyYwXbbn_STS-MvDMR_Me1EDFaWK37Qypt2SEknuBaw0q3gg4',
+          __RequestVerificationToken: requestVerificationToken,
         },
         {
           headers: {
@@ -1151,6 +1353,8 @@ export default function main() {
           },
         }
       )
+
+      requestVerificationToken = getRequestVerificationTokenValue(response);
 
       response = http.get(
         `${address}/lib/bootstrap/dist/css/bootstrap.min.css`,
@@ -1320,8 +1524,7 @@ export default function main() {
           questionIndex: '1',
           question: "Does the qualification name include 'Early Years Educator'?",
           Answer: 'yes',
-          __RequestVerificationToken:
-            'CfDJ8AK9tTBzYtFDpZlgo1wVLPwXnL4MQo-VI3NFL5e6yey5VtcIlyoI-Gq88rOMCB57Kmuj8qAfkj7jvmj4qtH1aJrvPGCn4e3lqTwTLGzYw4thkF2pNgXmYqcxyKBf6dnBOyFSzb6RCfpcSOeEo7qY0OI',
+          __RequestVerificationToken: requestVerificationToken,
         },
         {
           headers: {
@@ -1336,6 +1539,8 @@ export default function main() {
           },
         }
       )
+
+      requestVerificationToken = getRequestVerificationTokenValue(response);
 
       response = http.get(
         `${address}/lib/bootstrap/dist/css/bootstrap.min.css`,
@@ -1516,6 +1721,9 @@ export default function main() {
           },
         }
       )
+
+      requestVerificationToken = getRequestVerificationTokenValue(response);
+
       response = http.get(
         `${address}/lib/bootstrap/dist/css/bootstrap.min.css`,
         {
@@ -1682,6 +1890,14 @@ export default function main() {
     }
   )
 
-  // Automatically added sleep
   sleep(1)
+}
+
+function getRequestVerificationTokenValue(response) {
+
+  const requestVerificationTokenInput = response.html().find('input[name=__RequestVerificationToken]');
+
+  const requestVerificationToken = requestVerificationTokenInput.attr('value');
+
+  return requestVerificationToken;
 }
