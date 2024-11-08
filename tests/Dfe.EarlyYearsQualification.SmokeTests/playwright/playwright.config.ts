6d4538bda0e5b54c3f2fb26028c37ce2192f6846
Daniel Clarke <daniel.clarke@education.gov.uk>
2024-11-08 13:11:01 +0000

    Added new smoke tests in playwright with new github action

--- tests/Dfe.EarlyYearsQualification.SmokeTests/playwright/playwright.config.ts
+++ tests/Dfe.EarlyYearsQualification.SmokeTests/playwright/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:5025',
+    baseURL: process.env.WEBAPP_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -41,10 +41,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/Dfe.EarlyYearsQualification.SmokeTests/playwright/tests/smoke-tests.spec.ts
+++ tests/Dfe.EarlyYearsQualification.SmokeTests/playwright/tests/smoke-tests.spec.ts
@@ -1,41 +1,49 @@
 import { test, expect } from '@playwright/test';
 
 test.describe("A spec used to smoke test the environment once a deployment has happened", () => {
-  test.beforeEach(async ({ page , context}) => {
+  test("should return search results", async ({ page, context }) => {
+
+    // attempt to set cookie and navigate to start page
     await context.addCookies([
-      { name: 'auth-secret', value: process.env.AUTH_SECRET, path: '/', domain: 'localhost' }
+      { name: 'auth-secret', value: process.env.AUTH_SECRET, path: '/', domain: process.env.WEBAPP_URL }
     ]);
-    await page.goto(process.env.WEBAPP_URL + "/");
-    await expect(page.getByTestId("start-button")).toBeVisible();
-  });
-  
-  test("should return search results", async ({ page }) => {
+    
+    await page.goto("/");
+    
+    //if we end up navigated to the challenge page, then fill in the password and continue
+    if (page.url().includes("challenge")) {
+      await page.locator("#PasswordValue").type(process.env.AUTH_SECRET);
+      await page.locator("#question-submit").click();
+      await page.waitForURL("/");
+    }
+    
     // home page
+    await expect(page.getByTestId("start-button")).toBeVisible();
     await page.getByTestId("start-button").click();
-    await page.waitForURL("/questions/where-was-the-qualification-awarded");
 
     // where-was-the-qualification-awarded page
+    await expect(page.url()).toContain("/questions/where-was-the-qualification-awarded");
     await page.locator("#england").click();
     await page.locator("#question-submit").click();
-    await page.waitForURL("/questions/when-was-the-qualification-started");
 
     // when-was-the-qualification-started page
+    await expect(page.url()).toContain("/questions/when-was-the-qualification-started");
     await page.locator("#date-started-month").type("7");
     await page.locator("#date-started-year").type("2015");
     await page.locator("#question-submit").click();
-    await page.waitForURL("/questions/what-level-is-the-qualification");
 
     // what-level-is-the-qualification page
-    await page.locator("#0").click();
+    await expect(page.url()).toContain("/questions/what-level-is-the-qualification");
+    await page.locator('input[id="0"]').click();
     await page.locator("#question-submit").click();
-    await page.waitForURL("/questions/what-is-the-awarding-organisation");
 
     // what-is-the-awarding-organisation page
+    await expect(page.url()).toContain("/questions/what-is-the-awarding-organisation");
     await page.locator("#awarding-organisation-not-in-list").click();
     await page.locator("#question-submit").click();
-    await page.waitForURL("/qualifications");
 
     // qualifications page
+    await expect(page.url()).toContain("/qualifications");
     // If this shows then no qualifications are getting returned indicating possible issue
     await expect(page.locator("#no-result-content")).not.toBeVisible();
   });
