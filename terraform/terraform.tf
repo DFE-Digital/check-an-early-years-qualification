a1bb51593a933f007f71f040cb9c78d4fb0c4f81
RobertGHippo <robert.gammon@education.gov.uk>
2024-11-22 15:12:18 +0000

    Allow non-specific terraform patch version, but constrain maj and min version

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.71.0"
+      version = "~> 4.11.0"
     }
   }
 
-  required_version = ">= 1.5.0"
+  required_version = "~> 1.5.0"
 
   backend "azurerm" {
     use_oidc = true
