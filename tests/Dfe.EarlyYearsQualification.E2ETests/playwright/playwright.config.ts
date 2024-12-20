201aba3e823f26598b5695e28448e1a3512bc985
Sam Carter <sam.carter@education.gov.uk>
2024-12-20 10:54:40 +0000

    Try switching to using playwright webserver

--- tests/Dfe.EarlyYearsQualification.E2ETests/playwright/playwright.config.ts
+++ tests/Dfe.EarlyYearsQualification.E2ETests/playwright/playwright.config.ts
@@ -12,6 +12,9 @@ import {defineConfig, devices} from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 require('dotenv').config();
+
+const baseUrl = 'http://localhost:5025'
+
 export default defineConfig({
     testDir: './tests',
     /* Run tests in files in parallel */
@@ -27,7 +30,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: process.env.WEBAPP_URL,
+        baseURL: baseUrl,
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -75,9 +78,9 @@ export default defineConfig({
     ],
 
     /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
-    //   reuseExistingServer: !process.env.CI,
-    // },
+    webServer: {
+      command: 'cd ../../../src/Dfe.EarlyYearsQualification.Web && dotnet run --project ./Dfe.EarlyYearsQualification.Web.csproj --UseMockContentful=true',
+      url: baseUrl,
+      reuseExistingServer: !process.env.CI,
+    },
 });
