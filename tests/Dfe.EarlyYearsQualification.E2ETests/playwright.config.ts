95cc2260cc38d75240686e2113516c14c1d872e8
Tom Whittington <Tom@nooksrest.co.uk>
2025-01-17 14:40:50 +0000

    disabled webkit

--- tests/Dfe.EarlyYearsQualification.E2ETests/playwright.config.ts
+++ tests/Dfe.EarlyYearsQualification.E2ETests/playwright.config.ts
@@ -45,10 +45,12 @@ export default defineConfig({
             name: 'firefox',
             use: {...devices['Desktop Firefox']},
         },
+        /*
         {
             name: 'webkit',
             use: {...devices['Desktop Safari']},
         },
+        */
         /* Test against mobile viewports. */
         // {
         //   name: 'Mobile Chrome',
