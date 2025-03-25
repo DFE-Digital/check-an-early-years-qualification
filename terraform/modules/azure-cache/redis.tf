d6622413484499e8799b9ead48d26fb433f7a27f
RobertGHippo <robert.gammon@education.gov.uk>
2025-03-25 16:44:05 +0000

    Create basic cache

--- terraform/modules/azure-cache/redis.tf
+++ terraform/modules/azure-cache/redis.tf
@@ -1,6 +1,20 @@
-resource "azurerm_private_dns_zone" "redis_dns" {
-  name                = "redis.cache.windows.net"
-  resource_group_name = var.resource_group
+# Create Azure Cache for Redis
+resource "azurerm_redis_cache" "redis" {
+  name                               = "${var.resource_name_prefix}-redis-cache"
+  location                           = var.location
+  resource_group_name                = var.resource_group
+  capacity                           = var.environment != "development" ? 0 : 1
+  family                             = "C"
+  sku_name                           = var.environment != "development" ? "Standard" : "Basic"
+  access_keys_authentication_enabled = false
+  non_ssl_port_enabled               = false
+  minimum_tls_version                = "1.2"
+  public_network_access_enabled      = true
+
+  redis_configuration {
+    authentication_enabled                  = true
+    active_directory_authentication_enabled = true
+  }
 
   tags = var.tags
 
@@ -11,5 +25,4 @@ resource "azurerm_private_dns_zone" "redis_dns" {
       tags["Service Offering"]
     ]
   }
-
 }
