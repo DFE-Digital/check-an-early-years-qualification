a9d0a2c62e6265b954fc63cef5fe4e70e62d7193
RobertGHippo <robert.gammon@education.gov.uk>
2025-03-25 17:56:09 +0000

    Fixed typo

--- terraform/modules/azure-cache/private-dns.tf
+++ terraform/modules/azure-cache/private-dns.tf
@@ -28,7 +28,7 @@ resource "azurerm_private_endpoint" "redis_endpoint" {
 
   private_dns_zone_group {
     name                 = "default"
-    private_dns_zone_ids = [azure_private_dns_zone.redis_pdz.id]
+    private_dns_zone_ids = [azurerm_private_dns_zone.redis_pdz.id]
   }
 
   tags = var.tags
