f88d354c40c8e242210b618a1677006677017a79
RobertGHippo <robert.gammon@education.gov.uk>
2025-03-26 12:27:11 +0000

    add explicit vnet link

--- terraform/modules/azure-cache/private-dns.tf
+++ terraform/modules/azure-cache/private-dns.tf
@@ -13,6 +13,23 @@ resource "azurerm_private_dns_zone" "redis_pdz" {
   }
 }
 
+resource "azurerm_private_dns_zone_virtual_network_link" "redis_pdz_link" {
+  name                  = "${azurerm_redis_cache.redis.name}-plink"
+  resource_group_name   = var.resource_group
+  virtual_network_id    = var.vnet_id
+  private_dns_zone_name = azurerm_private_dns_zone.redis_pdz.name
+
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = [
+      tags["Environment"],
+      tags["Product"],
+      tags["Service Offering"]
+    ]
+  }
+}
+
 resource "azurerm_private_endpoint" "redis_endpoint" {
   name                = "${var.resource_name_prefix}-redis-pe"
   location            = var.location
