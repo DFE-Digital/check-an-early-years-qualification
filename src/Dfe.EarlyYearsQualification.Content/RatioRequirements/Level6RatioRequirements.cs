835eb3b36a4fedc17800ee21d8027456c9f09bc0
Sam Carter <sam.carter@education.gov.uk>
2025-05-09 13:34:46 +0100

    EYQB-1051: Initial commit to remove the F&R fields out of Contentful into code

---
+++ src/Dfe.EarlyYearsQualification.Content/RatioRequirements/Level6RatioRequirements.cs
@@ -0,0 +1,10 @@
+namespace Dfe.EarlyYearsQualification.Content.RatioRequirements;
+
+public class Level6RatioRequirements : BaseRatioRequirement
+{
+    public Level6RatioRequirements()
+    {
+        FullAndRelevantForQtsEtcBefore2014 = true;
+        FullAndRelevantForQtsEtcAfter2014 = true;
+    }
+}
