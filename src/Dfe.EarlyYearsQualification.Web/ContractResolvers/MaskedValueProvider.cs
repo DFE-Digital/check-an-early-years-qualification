f7e57ffd4ba0dd209891e5f627e0f361a191e16c
Sam C <156680559+sam-c-dfe@users.noreply.github.com>
2025-05-21 09:10:38 +0100

    Initial commit of the Qualification caching work (#609)
    * Initial commit of the Qualification caching work
    
    * Renamed filter
    
    * Removed mock query builder
    
    * EYQB-1016: Duplicate qualification names (#612)
    
    * Initial commit to add in AO next to the qualification name if there is a duplicate qualification name in the list
    
    * Fixed broken unit test
    
    * Added new touch icons
    
    * Updated as per Roberts suggestion
    
    * Updated to add where to the qualifications filter
    
    * Updated to add additional unit tests to increase coverage
    
    * Added excluded from coverage

--- src/Dfe.EarlyYearsQualification.Web/ContractResolvers/MaskedValueProvider.cs
+++ src/Dfe.EarlyYearsQualification.Web/ContractResolvers/MaskedValueProvider.cs
@@ -1,9 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
 using Newtonsoft.Json.Serialization;
 
 namespace Dfe.EarlyYearsQualification.Web.ContractResolvers;
 
 public class MaskedValueProvider : IValueProvider
 {
+    [ExcludeFromCodeCoverage(Justification = "This is not used")]
     public void SetValue(object target, object? value)
     {
         // We don't use this
