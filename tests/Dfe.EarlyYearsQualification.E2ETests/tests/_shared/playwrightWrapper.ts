4b8102edb6490c07d855fb889c128f4f1bdedb87
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2025-06-23 14:51:32 +0100

    chore: set viewport size in Playwright snapshot tests and update snapshot assets

--- tests/Dfe.EarlyYearsQualification.E2ETests/tests/_shared/playwrightWrapper.ts
+++ tests/Dfe.EarlyYearsQualification.E2ETests/tests/_shared/playwrightWrapper.ts
@@ -420,6 +420,7 @@ export async function checkRatiosHeading(page: Page, heading: string, body?: str
 }
 
 export async function checkSnapshot(page: Page) {
+    await page.setViewportSize({ width: 2000, height: 2000 });
     await expect(page).toHaveScreenshot({fullPage: true, maxDiffPixelRatio: 0.005});
 }
 
