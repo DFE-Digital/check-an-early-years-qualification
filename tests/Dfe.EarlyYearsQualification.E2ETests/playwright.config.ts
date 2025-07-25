54efe367b273a29bd98a58f18e702b2061e1e71c
Sam Carter <sam.carter@education.gov.uk>
2025-07-25 09:03:22 +0100

    Change config

--- tests/Dfe.EarlyYearsQualification.E2ETests/playwright.config.ts
+++ tests/Dfe.EarlyYearsQualification.E2ETests/playwright.config.ts
@@ -78,7 +78,6 @@ export default defineConfig({
     /* Run your local dev server before starting the tests */
     webServer: {
         command: `cd ../../src/Dfe.EarlyYearsQualification.Web && dotnet run --urls "${process.env.WEBAPP_URL}" --project ./Dfe.EarlyYearsQualification.Web.csproj --UseMockContentful=true --ServiceAccess:Keys:0="${process.env.AUTH_SECRET}"`,
-        url: process.env.WEBAPP_URL,
-        reuseExistingServer: true,
+        url: process.env.WEBAPP_URL
     },
 });
