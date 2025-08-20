a2f66be77dc57cc3b4e0929bc7199ddfa5f3b1d1
Sam Carter <sam.carter@education.gov.uk>
2025-08-20 14:08:42 +0100

    Refactored shared logic into base list renderer

--- src/Dfe.EarlyYearsQualification.Content/RichTextParsing/Renderers/UnorderedListRenderer.cs
+++ src/Dfe.EarlyYearsQualification.Content/RichTextParsing/Renderers/UnorderedListRenderer.cs
@@ -1,38 +1,14 @@
-﻿using System.Text;
-using Contentful.Core.Models;
-using Dfe.EarlyYearsQualification.Content.RichTextParsing.Helpers;
+﻿using Contentful.Core.Models;
 
 namespace Dfe.EarlyYearsQualification.Content.RichTextParsing.Renderers;
 
-public class UnorderedListRenderer : IContentRenderer
+public class UnorderedListRenderer : BaseListRenderer, IContentRenderer
 {
     public int Order { get; set; }
 
     public async Task<string> RenderAsync(IContent content)
     {
-        var list = content as List;
-
-        if (list!.Content.Count == 0)
-        {
-            return await Task.FromResult(string.Empty);
-        }
-
-        var sb = new StringBuilder();
-        sb.Append("<ul class=\"govuk-list govuk-list--bullet\">");
-
-        foreach (var contentValue in list.Content)
-        {
-            if (contentValue is not ListItem listItem)
-            {
-                continue;
-            }
-
-            var listItemParagraph = listItem.Content[0] as Paragraph;
-            sb.Append($"<li>{await NestedContentHelper.Render(listItemParagraph!.Content)}</li>");
-        }
-
-        sb.Append("</ul>");
-        return await Task.FromResult(sb.ToString());
+        return await RenderAsync(content, ListRendererType.UnorderedList);
     }
 
     public bool SupportsContent(IContent content)
