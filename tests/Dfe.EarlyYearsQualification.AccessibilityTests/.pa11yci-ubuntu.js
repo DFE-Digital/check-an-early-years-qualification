3ea68834cc99feb455a59510d591fc9f17c391cd
Sam Carter <sam.carter@education.gov.uk>
2025-09-10 09:01:06 +0100

    Updated config

--- tests/Dfe.EarlyYearsQualification.AccessibilityTests/.pa11yci-ubuntu.js
+++ tests/Dfe.EarlyYearsQualification.AccessibilityTests/.pa11yci-ubuntu.js
@@ -54,6 +54,7 @@ function createPa11yCiConfiguration(urls, defaults) {
     console.error('Env:', process.env.AUTH_SECRET);
 
     defaults.headers.Cookie = defaults.headers.Cookie.replace('${AUTH_SECRET}', process.env.AUTH_SECRET)
+    defaults.headers.Cookie = defaults.actions.entries().replace('${AUTH_SECRET}', process.env.AUTH_SECRET)
 
     return {
         defaults: defaults,
