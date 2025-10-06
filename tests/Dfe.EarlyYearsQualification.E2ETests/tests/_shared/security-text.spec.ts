a9f5e23042861fe456da78b63b6d9c7b9fc092af
Sam C <156680559+sam-c-dfe@users.noreply.github.com>
2025-10-06 10:07:35 +0100

    Package updates (#748)
    * Package updates
    
    * Changed security-text test to run under both e2e & smoke

--- tests/Dfe.EarlyYearsQualification.E2ETests/tests/_shared/security-text.spec.ts
+++ tests/Dfe.EarlyYearsQualification.E2ETests/tests/_shared/security-text.spec.ts
@@ -1,9 +1,9 @@
 ﻿import {test} from '@playwright/test';
-import {checkUrl, authorise} from '../../_shared/playwrightWrapper';
+import {checkUrl, authorise} from './playwrightWrapper';
 
 const expectedUrl = "https://vdp.security.education.gov.uk/.well-known/security.txt";
 
-test.describe('A spec used to test the security.txt redirects', {tag: "@e2e"}, () => {
+test.describe('A spec used to test the security.txt redirects', {tag: ["@e2e", "@smoke"]}, () => {
     ["/security.txt", "/.well-known/security.txt"].forEach((option) => {
         test(`Checks that going to ${option} redirects to correct location`, async ({page, context}) => {
             await authorise(context);
