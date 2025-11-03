525d05a8309ec366bd77ed948e9b7cb79565e6d7
Sam Carter <sam.carter@education.gov.uk>
2025-11-03 10:05:22 +0000

    Updated the selector and rule

--- terraform/modules/azure-web/app-gateway.tf
+++ terraform/modules/azure-web/app-gateway.tf
@@ -63,12 +63,17 @@ resource "azurerm_web_application_firewall_policy" "agw_wafp" {
       selector_match_operator = "StartsWith"
     }
 
-    /* The exclusion below stops the cookie banner hide functionality from triggering the 949110 - REQUEST-949-BLOCKING-EVALUATION rule.
-     As it contains the word select in the request arg, it thinks it's a SQLi attack. */
+    /* The exclusion below stops the cookie banner hide functionality from triggering a SQLi firewall rule.
+     If the user is on select-a-qualification-to-check, and as it contains the word select in the request arg, it thinks it's a SQLi attack. */
     exclusion {
       match_variable          = "RequestArgValues"
-      selector                = "/select-a-qualification-to-check"
+      selector                = "returnUrl"
       selector_match_operator = "Contains"
+      excluded_rule_set {
+        rule_group {
+          rule_group_name = "REQUEST-942-APPLICATION-ATTACK-SQLI"
+        }
+      }
     }
   }
 
