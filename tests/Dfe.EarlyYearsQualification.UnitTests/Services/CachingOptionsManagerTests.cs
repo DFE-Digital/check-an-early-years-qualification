3eecb80143bdae0a1550c12bae6bcdef6c384725
RobertGHippo <robert.gammon@education.gov.uk>
2026-01-06 14:15:02 +0000

    Correct indentation

--- tests/Dfe.EarlyYearsQualification.UnitTests/Services/CachingOptionsManagerTests.cs
+++ tests/Dfe.EarlyYearsQualification.UnitTests/Services/CachingOptionsManagerTests.cs
@@ -16,48 +16,48 @@ public class CachingOptionsManagerTests
                       {
                           { CachingOptionsManager.OptionsCookieKey, nameof(CachingOption.BypassCache) }
                       };
-    
+
         var cookieManager = new Mock<ICookieManager>();
         cookieManager.Setup(m => m.ReadInboundCookies())
                      .Returns(cookies);
-    
+
         var sut = new CachingOptionsManager(NullLogger<CachingOptionsManager>.Instance, cookieManager.Object);
-    
+
         var option = await sut.GetCachingOption();
-    
+
         option.Should().Be(CachingOption.BypassCache);
     }
-    
+
     [TestMethod]
     public async Task WhenCookiesIsNull_GetCachingOption_ReturnsDefault()
     {
         var cookieManager = new Mock<ICookieManager>();
         cookieManager.Setup(m => m.ReadInboundCookies())
                      .Returns((IDictionary<string, string>)null!);
-    
+
         var sut = new CachingOptionsManager(NullLogger<CachingOptionsManager>.Instance, cookieManager.Object);
-    
+
         var option = await sut.GetCachingOption();
-    
+
         option.Should().Be(CachingOption.UseCache);
     }
-    
+
     [TestMethod]
     public async Task WhenCookiesHasNoOptionValue_GetCachingOption_ReturnsDefault()
     {
         var cookies = new Dictionary<string, string>();
-    
+
         var cookieManager = new Mock<ICookieManager>();
         cookieManager.Setup(m => m.ReadInboundCookies())
                      .Returns(cookies);
-    
+
         var sut = new CachingOptionsManager(NullLogger<CachingOptionsManager>.Instance, cookieManager.Object);
-    
+
         var option = await sut.GetCachingOption();
-    
+
         option.Should().Be(CachingOption.UseCache);
     }
-    
+
     [TestMethod]
     public async Task WhenCookiesHasEmptyOptionValue_GetCachingOption_ReturnsDefault()
     {
@@ -65,19 +65,19 @@ public class CachingOptionsManagerTests
                       {
                           { CachingOptionsManager.OptionsCookieKey, "" }
                       };
-    
+
         var cookieManager = new Mock<ICookieManager>();
         cookieManager.Setup(m => m.ReadInboundCookies())
                      .Returns(cookies);
-    
+
         var sut =
             new CachingOptionsManager(NullLogger<CachingOptionsManager>.Instance, cookieManager.Object);
-    
+
         var option = await sut.GetCachingOption();
-    
+
         option.Should().Be(CachingOption.UseCache);
     }
-    
+
     [TestMethod]
     public async Task WhenCookiesHasUnexpectedOptionValue_GetCachingOption_ReturnsDefault()
     {
@@ -85,41 +85,41 @@ public class CachingOptionsManagerTests
                       {
                           { CachingOptionsManager.OptionsCookieKey, "NotAValidValue" }
                       };
-    
+
         var cookieManager = new Mock<ICookieManager>();
         cookieManager.Setup(m => m.ReadInboundCookies())
                      .Returns(cookies);
-    
+
         var sut = new CachingOptionsManager(NullLogger<CachingOptionsManager>.Instance, cookieManager.Object);
-    
+
         var option = await sut.GetCachingOption();
-    
+
         option.Should().Be(CachingOption.UseCache);
     }
-    
+
     [TestMethod]
     public async Task WhenCookiesHasUnexpectedOptionValue_GetCachingOption_LogsWarning()
     {
         const string invalidOptionValue = "NotAValidValue";
-    
+
         var cookies = new Dictionary<string, string>
                       {
                           { CachingOptionsManager.OptionsCookieKey, invalidOptionValue }
                       };
-    
+
         var cookieManager = new Mock<ICookieManager>();
         cookieManager.Setup(m => m.ReadInboundCookies())
                      .Returns(cookies);
-    
+
         var logger = new Mock<ILogger<CachingOptionsManager>>();
-    
+
         var sut = new CachingOptionsManager(logger.Object, cookieManager.Object);
-    
+
         await sut.GetCachingOption();
-    
+
         logger.VerifyWarning($@"User's caching option set to unexpected value '{invalidOptionValue}'");
     }
-    
+
     [TestMethod]
     public async Task WhenCookiesHasOptionValueDefault_GetCachingOption_ReturnsDefault()
     {
@@ -127,19 +127,18 @@ public class CachingOptionsManagerTests
                       {
                           { CachingOptionsManager.OptionsCookieKey, nameof(CachingOption.UseCache) }
                       };
-    
+
         var cookieManager = new Mock<ICookieManager>();
         cookieManager.Setup(m => m.ReadInboundCookies())
                      .Returns(cookies);
-    
+
         var sut = new CachingOptionsManager(NullLogger<CachingOptionsManager>.Instance, cookieManager.Object);
-    
+
         var option = await sut.GetCachingOption();
-    
+
         option.Should().Be(CachingOption.UseCache);
     }
-    
-    
+
     [TestMethod]
     public async Task SetCachingOption_Calls_CookieManager_SetOutboundCookie()
     {
@@ -147,11 +146,12 @@ public class CachingOptionsManagerTests
         var logger = new Mock<ILogger<CachingOptionsManager>>();
         var cookieManager = new Mock<ICookieManager>();
         var cachingOption = new CachingOption();
-    
+
         // Act
         await new CachingOptionsManager(logger.Object, cookieManager.Object).SetCachingOption(cachingOption);
-    
+
         // Assert
-        cookieManager.Verify(x => x.SetOutboundCookie(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CookieOptions>()));
+        cookieManager.Verify(x => x.SetOutboundCookie(It.IsAny<string>(), It.IsAny<string>(),
+                                                      It.IsAny<CookieOptions>()));
     }
 }
