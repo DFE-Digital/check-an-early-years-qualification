891a6581f5ea902e9b2fa7f32554995203fb96cb
RobertGHippo <robert.gammon@hippodigital.co.uk>
2026-03-10 14:27:35 +0000

    Doesn't like worker_count here

--- terraform/modules/azure-web/web-app.tf
+++ terraform/modules/azure-web/web-app.tf
@@ -101,8 +101,7 @@ resource "azurerm_linux_web_app" "webapp" {
       tags["hidden-link: /app-insights-conn-string"],
       tags["hidden-link: /app-insights-instrumentation-key"],
       tags["hidden-link: /app-insights-resource-id"],
-      site_config.0.application_stack,
-      worker_count
+      site_config.0.application_stack
     ]
   }
 
