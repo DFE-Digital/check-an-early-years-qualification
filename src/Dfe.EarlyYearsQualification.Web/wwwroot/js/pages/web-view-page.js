40da8a1b9ad9486d21990851d8d55106dda0b87d
jack saxby <jack.saxby@hotmail.com>
2026-04-01 13:46:23 +0100

    EYQB-1559 renamed webview GA event

--- src/Dfe.EarlyYearsQualification.Web/wwwroot/js/pages/web-view-page.js
+++ src/Dfe.EarlyYearsQualification.Web/wwwroot/js/pages/web-view-page.js
@@ -1,21 +1,21 @@
 $(window).on('load', function () {
-    let qualificationCount = $('.govuk-summary-card').length;
-    let keywordFilter = $('[value^="search-term"] span').first().text();
-    let startDateFilter = $('[value^="start-date"] span').first().text();
-    let qualificationLevelFilter = $('[value^="qualification-level"] span').first().text();
+    let totalQualifications = $('.govuk-summary-card').length;
+    let filterKeyword = $('[value^="search-term"] span').first().text();
+    let filterStartDate = $('[value^="start-date"] span').first().text();
+    let filterQualificationLevel = $('[value^="qualification-level"] span').first().text();
 
     window.dataLayer.push({
         'event': "webview-results-returned",
-        'keywordFilter': keywordFilter,
-        'startDateFilter': startDateFilter,
-        'qualificationLevelFilter': qualificationLevelFilter,
-        'totalQualifications': qualificationCount
+        'filterKeyword': filterKeyword,
+        'filterStartDate': filterStartDate,
+        'filterQualificationLevel': filterQualificationLevel,
+        'totalQualifications': totalQualifications
     });
 });
 
 $("#remove-filter-form").on("submit", function (event) {
     window.dataLayer.push({
         'event': 'webview-remove-filter',
-        'value': event.originalEvent.submitter.value.replace(/search-term-|start-date-|qualification-level-/g, ""),
+        'answer': event.originalEvent.submitter.value.replace(/search-term-|start-date-|qualification-level-/g, ""),
     });
 });
