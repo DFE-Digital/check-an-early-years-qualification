a100807df29b1d918545cf94a2a28518cd7fa133
oweynbrooke <oweyn.brooke@education.gov.uk>
2026-05-13 14:49:56 +0100

    Removing unnesscary usings

--- tests/Dfe.EarlyYearsQualification.UnitTests/Controllers/HelpControllerTests.cs
+++ tests/Dfe.EarlyYearsQualification.UnitTests/Controllers/HelpControllerTests.cs
@@ -1,6 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Dfe.EarlyYearsQualification.Content.Entities;
 using Dfe.EarlyYearsQualification.Content.Entities.Help;
 using Dfe.EarlyYearsQualification.Mock.Helpers;
