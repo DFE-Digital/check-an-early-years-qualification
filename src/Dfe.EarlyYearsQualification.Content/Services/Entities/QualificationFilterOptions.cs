b9505b1c6b7a0acddf9deaaeaa13433d042170f7
Sam Carter <sam.carter@education.gov.uk>
2026-08-13 14:50:39 +0100

    Changed setters to init

--- src/Dfe.EarlyYearsQualification.Content/Services/Entities/QualificationFilterOptions.cs
+++ src/Dfe.EarlyYearsQualification.Content/Services/Entities/QualificationFilterOptions.cs
@@ -2,11 +2,11 @@ namespace Dfe.EarlyYearsQualification.Content.Services.Entities;
 
 public class QualificationFilterOptions
 {
-    public int? Level { get; set; }
-    public int? StartDateMonth { get; set; }
-    public int? StartDateYear { get; set; }
-    public string? AwardingOrganisation { get; set; }
-    public string? QualificationName { get; set; }
-    public string? Nation { get; set; }
-    public bool IncludeAllQualifications { get; set; }
+    public int? Level { get; init; }
+    public int? StartDateMonth { get; init; }
+    public int? StartDateYear { get; init; }
+    public string? AwardingOrganisation { get; init; }
+    public string? QualificationName { get; init; }
+    public string? Nation { get; init; }
+    public bool IncludeAllQualifications { get; init; }
 }
